3dd0773b1ec3345d866dcfdaec27203e19135703
Fernando Bermudez <f3rbermudez@gmail.com>
2025-04-29 20:17:21 -0300

    Joinnow completado

--- src/app/components/JoinNow.tsx
+++ src/app/components/JoinNow.tsx
@@ -1,6 +1,6 @@
 export const JoinNow = () => {
   return (
-    <div className="mx-25 px-4 my-16 max-[1280px]:px-0 max-[1280px]:mx-4 bg-[#4E4B4B]  rounded-lg  border border-[#1B5055]">
+    <div className="mx-25 px-4 my-16 max-[1280px]:px-0 max-[1280px]:mx-4 bg-[#3BAFBB]/10  rounded-lg  border border-[#1B5055] hover:bg-[#3BAFBB]/20">
       <div className=" items-center justify-center  ">
         <div />
 
@@ -12,7 +12,7 @@ export const JoinNow = () => {
             Expand your business by selling with us. Join our partner program
             for increased visibility and sales opportunities.
           </p>
-          <button className=" text-[20px] border border-[#3BAFBB] rounded-lg bg-[#3BAFBB] h-14 w-48 cursor-pointer">
+          <button className=" text-[20px] border border-[#3BAFBB] rounded-lg bg-[#3BAFBB] hover:bg-[#3BAFBB]/80 h-14 w-48 cursor-pointer">
             Join Now!
           </button>
         </div>
