0d37701904a2d829507591d862c210d335897009
Fernando Bermudez <f3rbermudez@gmail.com>
2025-06-15 15:40:42 -0300

    feat: mejorar componente SearchBar con selección de rango de fechas y ubicación
    - Se ha actualizado el estado del componente para manejar un rango de fechas en lugar de una sola fecha.
    - Se ha añadido un nuevo campo para seleccionar la ubicación, con una lista desplegable de departamentos.
    - Se han realizado ajustes en la interfaz para mostrar el rango de fechas seleccionado y mejorar la usabilidad del campo de ubicación.

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -6,6 +6,29 @@ import { CaretDown } from '@phosphor-icons/react';
 import { Calendar } from '@/components/ui/calendar';
 import { es } from 'date-fns/locale';
 import { format } from 'date-fns';
+import { type DateRange } from 'react-day-picker';
+
+const departments = [
+  'Artigas',
+  'Canelones',
+  'Cerro Largo',
+  'Colonia',
+  'Durazno',
+  'Flores',
+  'Florida',
+  'Lavalleja',
+  'Maldonado',
+  'Montevideo',
+  'Paysandú',
+  'Río Negro',
+  'Rivera',
+  'Rocha',
+  'Salto',
+  'San José',
+  'Soriano',
+  'Tacuarembó',
+  'Treinta y Tres',
+];
 
 interface SearchBarProps {
   onSearch: (searchTerm: string) => void;
@@ -14,9 +37,12 @@ interface SearchBarProps {
 export const SearchBar = ({ onSearch }: SearchBarProps) => {
   const [isSticky, setIsSticky] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
-  const [date, setDate] = useState<Date | undefined>(new Date(Date.now()));
+  const [date, setDate] = useState<DateRange | undefined>();
   const [isCalendarOpen, setIsCalendarOpen] = useState(false);
   const calendarRef = useRef<HTMLDivElement>(null);
+  const [location, setLocation] = useState('');
+  const [isLocationOpen, setIsLocationOpen] = useState(false);
+  const locationRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
@@ -25,6 +51,12 @@ export const SearchBar = ({ onSearch }: SearchBarProps) => {
         !calendarRef.current.contains(event.target as Node)
       ) {
         setIsCalendarOpen(false);
+      }
+      if (
+        locationRef.current &&
+        !locationRef.current.contains(event.target as Node)
+      ) {
+        setIsLocationOpen(false);
       }
     }
 
@@ -49,9 +81,8 @@ export const SearchBar = ({ onSearch }: SearchBarProps) => {
     setSearchTerm(value);
     onSearch(value);
   };
-  const handleDateSelect = (selectedDate: Date | undefined) => {
+  const handleDateSelect = (selectedDate: DateRange | undefined) => {
     setDate(selectedDate);
-    setIsCalendarOpen(false);
   };
 
   return (
@@ -107,7 +138,16 @@ export const SearchBar = ({ onSearch }: SearchBarProps) => {
             <input
               type="text"
               placeholder="Date"
-              value={date ? format(date, 'dd/MM/yyyy') : ''}
+              value={
+                date?.from
+                  ? date.to
+                    ? `${format(date.from, 'dd/MM/yyyy')} - ${format(
+                        date.to,
+                        'dd/MM/yyyy'
+                      )}`
+                    : format(date.from, 'dd/MM/yyyy')
+                  : ''
+              }
               readOnly
               className="placeholder:text-[#3BAFBB] text-[#3BAFBB] w-full outline-none bg-transparent cursor-pointer"
               onClick={() => setIsCalendarOpen(!isCalendarOpen)}
@@ -120,7 +160,7 @@ export const SearchBar = ({ onSearch }: SearchBarProps) => {
               className="bg-[#1C1A1A] absolute w-full top-full left-0 mt-5 ml-1.5 z-50 transform transition-all duration-200 ease-in-out opacity-100 scale-100 shadow-lg max-sm:fixed max-sm:left-1/2 max-sm:-translate-x-1/2 max-sm:top-1/2 max-sm:-translate-y-1/2"
             >
               <Calendar
-                mode="single"
+                mode="range"
                 selected={date}
                 onSelect={handleDateSelect}
                 className="rounded-md border w-full bg-[#3BAFBB1A] text-[#3BAFBB] border-[#3BAFBB] shadow-xl max-sm:w-[calc(100vw-2rem)]"
@@ -131,18 +171,50 @@ export const SearchBar = ({ onSearch }: SearchBarProps) => {
           )}
         </div>
 
-        <div className="border-l-[#3BAFBB33] border-l-2 flex items-center gap-3 justify-center pl-2 w-full">
+        <div
+          ref={locationRef}
+          className="relative border-l-[#3BAFBB33] border-l-2 flex items-center gap-3 justify-center pl-2 w-full"
+        >
           <Image
             src="/assets/icons/search_bar/location.svg"
             alt="location"
             width={16}
             height={16}
           />
-          <input
-            type="text"
-            placeholder="Location"
-            className="placeholder:text-[#3BAFBB] text-[#3BAFBB] w-full outline-none bg-transparent"
-          />
+          <div
+            className="w-full relative"
+            onClick={() => setIsLocationOpen(!isLocationOpen)}
+          >
+            <input
+              type="text"
+              placeholder="Location"
+              value={location}
+              readOnly
+              className="placeholder:text-[#3BAFBB] text-[#3BAFBB] w-full outline-none bg-transparent cursor-pointer"
+            />
+            <CaretDown
+              className="text-[#3BAFBB] absolute right-0 top-1/2 -translate-y-1/2"
+              size={32}
+            />
+          </div>
+          {isLocationOpen && (
+            <div className="absolute w-full top-full left-0 mt-5 z-50 bg-[#1C1A1A] border border-[#3BAFBB] rounded-lg shadow-lg">
+              <ul className="max-h-60 overflow-y-auto">
+                {departments.map(dep => (
+                  <li
+                    key={dep}
+                    className="p-2 text-[#3BAFBB] hover:bg-[#3BAFBB33] cursor-pointer"
+                    onClick={() => {
+                      setLocation(dep);
+                      setIsLocationOpen(false);
+                    }}
+                  >
+                    {dep}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
 
         {/* <button
