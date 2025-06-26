5533d9810ef83304561e81958482a0d4a610af63
Fernando Bermudez <f3rbermudez@gmail.com>
2025-06-25 22:13:39 -0300

    feat: actualizar configuración de Next.js y refactorizar componente HomePage
    - Se ha añadido soporte para imágenes remotas desde imgur en la configuración de Next.js.
    - Se ha refactorizado el componente HomePage para utilizar un nuevo modelo de eventos, incluyendo la lógica para obtener eventos desde una API.
    - Se han actualizado los filtros y la presentación de eventos en el componente, mejorando la experiencia del usuario.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
