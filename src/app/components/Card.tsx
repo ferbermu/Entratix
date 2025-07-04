9ac9003e5f9eb7cb91fd96d182d61a6f13a66483
Fernando Bermudez <f3rbermudez@gmail.com>
2025-07-03 21:09:23 -0300

    feat: agregar clase para truncar texto y mejorar estilos en el componente Card
    - Se ha añadido una clase CSS personalizada `.truncate-2` para truncar texto a 2 líneas, mejorando la presentación del título en el componente Card.
    - Se han realizado ajustes en la estructura del componente Card, incluyendo cambios en el espaciado y la alineación de los elementos.

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -40,29 +40,30 @@ export const Card = ({
         <div className="absolute inset-0 bg-gradient-to-t from-black/90 via-black/50 to-transparent" />
       </div>
 
-      <div className="p-6 gap-6 text-white flex flex-col h-full">
-        <h3 className="text-3xl   ">{title}</h3>
+      <div className="uppercase p-6 gap-6 text-white flex flex-col h-70 justify-between">
+        <h3 className="text-3xl h-20  truncate-2">{title}</h3>
+        <div className="gap-6 flex flex-col text-md">
+          <div className="flex items-center gap-2  text-gray-300">
+            <Image
+              src={addressIcon}
+              alt="Location icon"
+              width={16}
+              height={16}
+              className="opacity-75"
+            />
+            <span className="text-xs">{address}</span>
+          </div>
 
-        <div className="flex items-center gap-2 text-md text-gray-300">
-          <Image
-            src={addressIcon}
-            alt="Location icon"
-            width={16}
-            height={16}
-            className="opacity-75"
-          />
-          <span className="text-xs">{address}</span>
-        </div>
-
-        <div className="flex items-center gap-2 text-md text-gray-300 ">
-          <Image
-            src={dateIcon}
-            alt="Calendar icon"
-            width={16}
-            height={16}
-            className="opacity-75"
-          />
-          <span className="text-xs">{date}</span>
+          <div className="flex items-center gap-2 text-md text-gray-300 ">
+            <Image
+              src={dateIcon}
+              alt="Calendar icon"
+              width={16}
+              height={16}
+              className="opacity-75"
+            />
+            <span className="text-xs">{date}</span>
+          </div>
         </div>
 
         <button className="mt-2 w-full px-3 py-3 bg-[#4E4B4B] text-white rounded-lg transition-colors duration-300 group-hover:bg-[#3BAFBB] cursor-pointer">
